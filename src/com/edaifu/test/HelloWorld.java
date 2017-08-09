94ccde160977242c7007d50f2ba19d3cce716a63
joker0017 <1102405354@qq.com>
2017-08-09 18:23:40 +0800

    first  submit

--- src/com/edaifu/test/HelloWorld.java
+++ src/com/edaifu/test/HelloWorld.java
@@ -5,6 +5,6 @@ package com.edaifu.test;
  */
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println(1111);
+        System.out.println(222222);
     }
 }
